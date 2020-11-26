9f24f6191aec4a398d3148f859a6572a27f1d985
Melbin <melbin97@hotmail.com>
2020-11-26 14:45:52 +0530

    Add project files.

---
+++ GothamCares.Entity/Food.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GothamCares.Entity
+{
+    public class Food
+    {
+    }
+}
---
+++ GothamCares.Entity/Outlet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GothamCares.Entity
+{
+    class Outlet
+    {
+    }
+}
