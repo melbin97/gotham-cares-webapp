79ae0b94ba01b0300a0dcd8d8c533303518e404f
benaiah-john <benaiah.john@experionglobal.com>
2020-11-30 12:13:27 +0530

    Added APIs

---
+++ GothamCares.Web/Controllers/OutletsControler.cs
@@ -0,0 +1,33 @@
+﻿using GothamCares.BLL;
+using GothamCares.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GothamCares.Web.Controllers
+{    
+    [ApiController]
+    public class OutletsControler : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/outlets")]
+        public async Task<IActionResult> GetOutlets()
+        {
+            OutletBLL outletBLL = new OutletBLL();
+            var outlets = await outletBLL.GetAllOutlets();
+
+            return Ok(outlets);
+        }
+
+        [HttpPost]
+        [Route("api/outlets")]
+        public IActionResult CreateOutlet([FromBody] Outlet outlet)
+        {            
+            return Ok();
+        }
+
+    }
+}
